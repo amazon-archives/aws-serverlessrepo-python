cf1dfc28da0201fc1df6b51ecb1de844c810d218
Vicky Wang <qwang@amazon.com>
2018-11-25 22:09:25 -0800

    Add Makefile, setup.py, Pipfile, and configuration files

---
+++ Makefile
@@ -0,0 +1,22 @@
+init:
+	pip install pipenv --upgrade
+	pipenv sync --dev
+
+test:
+	# Run unit tests, fail if coverage falls below 95%
+	pipenv run pytest --cov serverlessrepo --cov-report term-missing --cov-fail-under 95 tests/unit
+
+flake:
+	# Make sure code conforms to PEP8 standards
+	pipenv run flake8 serverlessrepo
+	pipenv run flake8 tests
+
+lint:
+	# Linter performs static analysis to catch latent bugs
+	pipenv run pylint --rcfile .pylintrc serverlessrepo
+
+# Command to run everytime you make changes to verify everything works
+build: flake lint test
+
+# Verifications to run before sending a pull request
+pr: init build
---
+++ setup.py
@@ -0,0 +1,70 @@
+"""A setuptools based setup module."""
+
+from io import open
+from os import path
+from setuptools import setup, find_packages
+
+# Required packages for this module to work
+REQUIRED = [
+    'pyyaml',
+    'boto3',
+    'six'
+]
+
+# Optional packages for this module to work
+EXTRAS = {
+    'dev': [
+        'pytest',
+        'pytest-cov',
+        'pylint',
+        'mock',
+        'flake8',
+        'flake8-docstrings'
+    ]
+}
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+about = {}
+with open(path.join(here, 'serverlessrepo', '__version__.py')) as f:
+    exec(f.read(), about)
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    license=about['__license__'],
+    keywords="AWS Serverless Application Repository",
+    packages=find_packages(exclude=['tests', 'docs']),
+    # Support Python 2.7 and 3.6 or greater
+    python_requires=(
+        '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*'
+    ),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Utilities',
+    ]
+)
