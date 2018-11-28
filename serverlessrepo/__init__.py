b0638bcd0842b613455aaad401909f10d79eed51
Vicky Wang <paoptu023@gmail.com>
2018-11-28 13:35:12 -0800

    Add Makefile, setup.py, Pipfile, and configuration files (#9)

--- serverlessrepo/__init__.py
+++ serverlessrepo/__init__.py
@@ -2,5 +2,9 @@
 Common library for AWS Serverless Application Repository
 """
 
-from .publish import publish_application
-from .permission_helper import make_application_public, make_application_private, share_application_with_accounts
+from .publish import publish_application  # noqa: F401
+from .permission_helper import (  # noqa: F401
+    make_application_public,
+    make_application_private,
+    share_application_with_accounts
+)
