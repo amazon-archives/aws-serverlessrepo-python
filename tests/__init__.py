cc1ff4e6615f4be9c682c06858bdeaf42e4f34d1
Vicky Wang <qwang@amazon.com>
2018-12-05 12:11:04 -0800

    Remove unnecessary system path config

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,4 @@
-import sys
 import os
-
-PATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, PATH + '/../serverlessrepo/')
 
 # set expected aws region environment variable
 os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
